c77b8086d433965672a756aae64feea212639a82
RMadya <kereyoben75@gmail.com>
2024-05-04 20:40:09 +0700

    void menambah node

--- DoubleLinkedList_143/DoubleLinkedList_143.cpp
+++ DoubleLinkedList_143/DoubleLinkedList_143.cpp
@@ -5,7 +5,18 @@ struct Node
 {
     int noMhs;
     string name;
-    Node *next;
-    Node *prev;
+    Node* next;
+    Node* prev;
 };
 
+Node* START = NULL;
+
+void addNode()
+{
+    Node* newNode = new Node();
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs;
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;
+
+    
